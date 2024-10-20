1d1a34d31a25371c31e91f77a60e2a416df5b0e5
motsan2560 <112244112+Motsan-Git@users.noreply.github.com>
2024-10-20 20:15:06 +0330

    initial commit

---
+++ MergeSortedArray/Main.java
@@ -0,0 +1,7 @@
+import MergeSortedArray.Solution;
+
+public static void main(String[] args) {
+        Solution solution = new Solution();
+
+    }
+
--- MergeSortedArray/Solution.java
+++ MergeSortedArray/Solution.java
@@ -1,7 +1,7 @@
 package MergeSortedArray;
 
 public class Solution {
-    public static void main(String[] args) {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
 
     }
 }
